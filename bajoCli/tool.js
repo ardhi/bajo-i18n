0ce7f6cb7785c5755f8d4f71fce53033dcb23433
Ardhi Lukianto <ardhi@lukianto.com>
2024-04-24 06:51:40 +0700

    Updates

--- bajoCli/tool.js
+++ bajoCli/tool.js
@@ -1,7 +1,1 @@
-async function tool ({ path, args = [] }) {
-  const { currentLoc } = this.bajo.helper
-  const { runToolMethod } = this.bajoCli.helper
-  await runToolMethod({ path, args, dir: `${currentLoc(import.meta).dir}/tool` })
-}
-
-export default tool
+export default 'defCliHandler'
