a432e9173703a91ac462b12d9d40c2af07c2b477
Ardhi Lukianto <ardhi@lukianto.com>
2024-07-14 19:29:29 +0700

    Update init.js

--- bajo/init.js
+++ bajo/init.js
@@ -6,16 +6,20 @@ async function init () {
   const spp = await sprintfPostProcessor.call(this)
   const { map, uniq, cloneDeep } = this.app.bajo.lib._
   const config = cloneDeep(this.config)
-  config.lng = this.app.bajo.config.lang
-  config.fallbackLng = this.app.bajo.config.lang
+  config.lang = this.app.bajo.config.lang
   config.supportedLngs = uniq(map(this.config.supportedLngs, l => l.split('-')[0]))
   config.nonExplicitSupportedLngs = true
+  if (!config.supportedLngs.includes(config.lang)) {
+    this.log.warn('Unsupported locale \'%s\', switched to the default one \'%s\'', config.lang, config.fallbackLng)
+    config.lang = config.fallbackLng
+  }
   this.config = config
   this.app.bajo.freeze(this.config)
   const opts = this.getConfig()
+  opts.lng = opts.lang
   await collectResources.call(this, opts)
   opts.defaultNS = 'main'
-  opts.fallbackNS = ['main', 'bajo']
+  opts.fallbackNS = ['main', 'bajo', 'bajoI18N']
   await i18next.use(spp).init(opts)
   this.instance = i18next
   this.log.debug('Internationalization is active now, locale: %s', opts.lng)
